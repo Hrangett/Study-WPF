6b8c880ca734230d48bce83c8edfe78cef853290
Hrangett <orangett217@gmail.com>
2022-06-07 12:50:08 +0900

    20220607 mvvm 오전 커밋

---
+++ day05/WpfMvvmApp/Helpers/Commons.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace WpfMvvmApp.Helpers
+{
+    public class Commons
+    {
+        /// <summary>
+        /// 이메일 검증
+        /// </summary>
+        /// <param name="email"></param>
+        /// <returns> true : 이메일 아님 </returns>
+        public static bool IsValidEmail(string email)
+        {
+            //c# verbatim :: 축자 문자열 사용에서 기대되는 동작대로, 역슬래시 \ 는 escape 문자로 취급되지 않는다. 또한 보간 문자열 사용으로 인해 기대되는 동작대로, 중괄호로 둘러싸여진 표현식은 해당 위치에 문자열로 삽입되기 전에 실제 값이 평가 (evaluate) 된다.
+            return Regex.IsMatch(email, @"[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?");
+        }
+
+        /// <summary>
+        /// 나이계산
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static int CalcAge(DateTime value)
+        {
+            int middle;
+            if (DateTime.Now.Month < value.Month || DateTime.Now.Month == value.Month && DateTime.Now.Day < value.Day)
+                middle = DateTime.Now.Year - value.Year - 1;
+            else
+                middle = DateTime.Now.Year - value.Year;
+
+            return middle;
+
+
+        }
+    }
+}
---
+++ day05/WpfMvvmApp/Views/RelayCommand.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfMvvmApp.Views
+{
+    /// <summary>
+    /// ViewMode과 View를 Glue하기위한 클래스
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    class RelayCommand<T> : ICommand    
+    {
+        private readonly Action<T> execute; //실행처리를 위한 Generic
+        private readonly Predicate<T> canExecute;   //실행여부를 판단하기위한 Generic
+
+
+        /// <summary>
+        /// execute, canExecute를 파라미터로 받는 생성자
+        /// </summary>
+        /// <param name="execute"></param>
+        /// <param name="canExecute"></param>
+        public RelayCommand(Action<T> execute, Predicate<T> canExecute = null)
+        {
+            this.execute = execute ?? throw new ArgumentException(nameof(execute));
+            this.canExecute = canExecute;
+        }
+
+        /// <summary>
+        /// execute만 파라미터로 받는 생성자
+        /// </summary>
+        /// <param name="execute"></param>
+        public RelayCommand(Action<T> execute) : this(execute, null)
+        {
+        }
+
+        //실행여부에 따라서 이벤트를 추가해주거나 삭제하는 이벤트 핸들러
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                CommandManager.RequerySuggested += value;
+            }
+            remove
+            {
+                CommandManager.RequerySuggested -= value;
+
+            }
+
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            //특정조건 충족 시 버튼 활성화 하도록 도와주는 메소드
+            return canExecute?.Invoke((T)parameter) ?? true;
+
+        }
+
+        public void Execute(object parameter)
+        {
+            execute((T)parameter);
+        }
+    }
+}
