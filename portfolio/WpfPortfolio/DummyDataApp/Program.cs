9cac96fc0f06567accdbd03ed052c49c41bfd929
Hrangett <orangett217@gmail.com>
2022-06-09 16:55:15 +0900

    20220609 Smarthome portfolio 시작

--- portfolio/WpfPortfolio/DummyDataApp/Program.cs
+++ portfolio/WpfPortfolio/DummyDataApp/Program.cs
@@ -6,32 +6,81 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using uPLibrary.Networking.M2Mqtt;
 
 namespace DummyDataApp
 {
-    class Program
+    public class Program
     {
+        public static string MqttBrokerUrl { get; set; }//static메소드에 상ㅇ하니까 static 선언 별 의미 없음
+        public static MqttClient Client { get; set; }
+        private static Thread MqttThread { get; set; }
+        private static Faker<SensorInfo> SensorData { get; set; }
+        private static string CurrValue { get; set; }
+
+
         static void Main(string[] args)
         {
-            int num = 1;
-            while (true)
-            {
-                var Rooms = new[] { "DINING", "LIVING", "BATH", "BED" };    //부엌,거실,욕실,침실
-                var sensorDummy = new Faker<SensorInfo>()
+    /* ================================================================================ */
+
+            InitializeConfig();     //구성초기화
+            ConnectMqttBroker();    //브로커 접속
+            StaticPublish();        //배포(Publish 발행)
+
+    /* ================================================================================ */
+
+
+        }
+
+        private static void InitializeConfig()
+        {
+            //구성 초기화
+            MqttBrokerUrl = "210.119.12.71";
+            var Rooms = new[] { "DINING", "LIVING", "BATH", "BED" };    //부엌,거실,욕실,침실
+            SensorData = new Faker<SensorInfo>()
                     .RuleFor(o => o.DevId, f => f.PickRandom(Rooms))
                     .RuleFor(r => r.CurrTime, f => f.Date.Past(0))//0을 넣으면 현재시간을 넣을 수 있다
                     .RuleFor(r => r.Temp, f => f.Random.Float(19.0f, 30.9f))
                     .RuleFor(r => r.Humid, f => f.Random.Float(40.0f, 63.9f));
 
-                var currValue = sensorDummy.Generate();
 
+        }
 
-                Console.WriteLine(num);
-                Console.WriteLine(JsonConvert.SerializeObject(currValue, Formatting.Indented));
-                num++;
+        private static void ConnectMqttBroker()
+        {
+            
+            try
+            {
+                Client = new MqttClient(MqttBrokerUrl);
+                Client.Connect("SMARTHOME01");    //connect 할 client의 이름 적기
+                Console.WriteLine("접속 성공");
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine($"접속 불가 :: {ex}");
+                Environment.Exit(5);
+            }
+        }
+
+        private static void StaticPublish()
+        {
+            MqttThread = new Thread(() => LoopPublish());
+            MqttThread.Start();
+
+        }
+
+        private static void LoopPublish()
+        {
+            while (true)
+            {
+                SensorInfo tempValue = SensorData.Generate();
+                CurrValue = JsonConvert.SerializeObject(tempValue, Formatting.Indented);
+                Client.Publish("home/device/fakedata", Encoding.Default.GetBytes(CurrValue));
+                Console.WriteLine($"Publiched : {CurrValue}");
                 Thread.Sleep(1000);
             }
-                
+
         }
     }
 }
---
+++ portfolio/WpfPortfolio/WpfSmartHomeMonitoringApp/ViewModels/DataBaseViewModel.cs
@@ -0,0 +1,150 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Text;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+using WpfSmartHomeMonitoringApp.Helpers;
+
+namespace WpfSmartHomeMonitoringApp.ViewModels
+{
+    public class DataBaseViewModel : Screen
+    {
+        private string brokerUrl;
+        private string topic;
+        private string connString;
+        private string dbLog;
+        private bool isConnected;
+
+        public string BrokerUrl
+        {
+            get { return brokerUrl; }
+            set
+            {
+                brokerUrl = value;
+                NotifyOfPropertyChange(() => BrokerUrl);
+
+            }
+        }
+
+        
+        public string Topic
+        {
+            get { return topic; }
+            set
+            {
+                topic = value;
+                NotifyOfPropertyChange(() => Topic);
+            }
+        }
+        
+
+       
+        public string ConnString
+        {
+            get => connString; 
+            
+            set
+            {
+                connString = value;
+                NotifyOfPropertyChange(() => ConnString);
+            }
+        }
+
+        
+        public string DbLog
+        {
+            get => dbLog; 
+            set
+            {
+                dbLog = value;
+                NotifyOfPropertyChange(() => DbLog);
+            }
+        }
+
+        public bool IsConnected
+        {
+            get => isConnected; 
+            set
+            {
+                isConnected = value;
+                NotifyOfPropertyChange(() => IsConnected);
+            }
+        }
+
+        public DataBaseViewModel()
+        {
+            BrokerUrl = Commons.BROKERHOST = "127.0.0.1";   //MQTT Broker IP 설정
+            Topic = Commons.PUB_TOPIC = "home/device/fakedata";
+            ConnString = Commons.CONNSTRING = "Data Source=PC01;Initial Catalog=OpenApiLab;Integrated Security=True";
+
+            if(Commons.IS_CONNECT)
+            {
+                IsConnected = true;
+            }
+
+        }
+
+
+        /// <summary>
+        /// DB연결 + MQTT 접속
+        /// </summary>
+        public void ConnectDb()
+        { 
+            if(IsConnected)
+            {
+                Commons.MQTT_CLIENT = new MqttClient(BrokerUrl);
+
+                try
+                {
+                    if(Commons.MQTT_CLIENT.IsConnected != true)
+                    {
+                        Commons.MQTT_CLIENT.MqttMsgPublishReceived += MQTT_CLIENT_MqttMsgPublishReceived;
+                        Commons.MQTT_CLIENT.Connect("MONITOR");
+                        Commons.MQTT_CLIENT.Subscribe(new string[] { Commons.PUB_TOPIC },
+                            new byte[] { MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE });
+                        
+                        UpdateText(">>> MQTT Broker Connected");
+                        IsConnected = Commons.IS_CONNECT = true;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    //Pass
+                    
+                }
+
+            }
+            else
+            {
+                try
+                {
+                    if(Commons.MQTT_CLIENT.IsConnected)
+                    {
+                        Commons.MQTT_CLIENT.MqttMsgPublishReceived -= MQTT_CLIENT_MqttMsgPublishReceived;
+                        Commons.MQTT_CLIENT.Disconnect();
+                        UpdateText(">>> MQTT Broker Disconnected");
+                        IsConnected = Commons.IS_CONNECT = false;
+                    }
+                }
+                catch (Exception)
+                {
+
+                    //throw;
+                }
+            }
+
+        }
+
+        private void UpdateText(string message)
+        {
+            DbLog += $"{message}\n";
+        }
+
+        private void MQTT_CLIENT_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+        {
+            var message = Encoding.UTF8.GetString(e.Message);   //bite형식으로 날라온 글짜 다시 string 형으로 바꿔
+            UpdateText(message);
+
+        }
+    }
+}
